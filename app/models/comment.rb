dc4b3c3d794eb1e8d0efa29b7e2e928d84c5d068
jimmy <jimmy.huangjin@gmail.com>
2017-12-01 11:23:01 +0800

    add floor number

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :article
 
   validates :content, presence: true, length: { maximum: 500 }
+
+  def floor_number
+    article.comments.index(self).to_i + 1
+  end
 end
