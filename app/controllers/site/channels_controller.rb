234d44f3372b60d661130213517fbf9824cff123
jimmy <jimmy.huangjin@gmail.com>
2017-12-04 21:23:11 +0800

    refine

--- app/controllers/site/channels_controller.rb
+++ app/controllers/site/channels_controller.rb
@@ -6,14 +6,14 @@ class Site::ChannelsController < Site::ApplicationController
   caches_action :show, :cache_path => Proc.new { |c| c.request.url + '-desktop' }, :expires_in => 6.hours
 
   def index
-    @channels = Channel.order('sortrank DESC, id DESC').paginate(page: params[:page], per_page: 300, total_entries: 1000000)
+    @channels = Channel.order('sortrank DESC, id DESC').paginate(page: params[:page], per_page: 300)
 
     set_meta title: '热门专题'
   end
 
   def show
     @channel = Channel.find_by!(slug: params[:slug])
-    @articles = @channel.articles.order('id DESC').paginate(page: params[:page], per_page: 20, total_entries: 100000)
+    @articles = @channel.articles.order('id DESC').paginate(page: params[:page], per_page: 20)
 
     @channel_keywords = @channel.seo_keywords
 
