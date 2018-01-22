367efe13036cc3775ee21cd1d989e24bc5fc806a
jimmy <jimmy.huangjin@gmail.com>
2018-01-22 21:50:49 +0800

    refine

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,13 +4,20 @@ class Api::V1::ArticlesController < Api::V1::ApplicationController
 
     per_page = (params[:per_page].presence || 20).to_i
     per_page = 100 if per_page > 100
-    
+
     @articles = if params[:category_id].blank?
       []
     elsif !nodes.include?(params[:category_id].to_i)
       []
     else
-      Article.where(approved: true).order('id DESC').paginate(page: params[:page], per_page: per_page)
+      node = Node.find_by(id: params[:category_id])
+      return [] if node.blank?
+
+    @articles = Article.where(node_id: node.self_and_descendants.pluck(:id))
+                       .where(approved: true)
+                       .order('id DESC')
+                       .paginate(page: params[:page], per_page: 20, total_entries: 1000000)
+
     end
   end
 end
