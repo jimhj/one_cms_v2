a45eff9b1d72e314f1dc72a00d16ac2cfcee44b8
jimmy <jimmy.huangjin@gmail.com>
2018-01-24 20:32:29 +0800

    update api

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::CategoriesController < Api::V1::ApplicationController
   def index
     @categories = Node.all.select{ |node| node.api_node.to_i == 1 }.collect do |cate|
-      {
-        category_id: cate.id,
-        name: cate.name,
-        short_name: cate.short_name,
-        articles: "https://www.7234.cn/api/v1/category/#{cate.id}/page/1"
-      }
+      node = cate_to_hash(cate)
+      children = cate.children.collect { |child| cate_to_hash(child) }
+      node.merge(children: children)
     end
 
     render json: @categories
+  end
+
+  private
+
+  def cate_to_hash(cate)
+    {
+      category_id: cate.id,
+      name: cate.name,
+      short_name: cate.short_name,
+      articles: "https://www.7234.cn/api/v1/category/#{cate.id}/page/1"
+    }
   end
 end
--- app/views/api/v1/articles/index.json.jbuilder
+++ app/views/api/v1/articles/index.json.jbuilder
@@ -1,8 +1,11 @@
 json.array! @articles do |article|
+  photo_url = (article.pictures.first || '').gsub('!thumb', '').sub('//', 'https://').presence || nil
   json.title article.title
   json.description article.seo_description
   json.created_at article.created_at.strftime('%F %T')
   json.url mip_article_url(article).sub('http://', 'https://')
   json.from article.source
-  json.photo_url (article.pictures.first || '').gsub('!thumb', '').sub('//', 'https://').presence || nil
+  json.comments_count article.comments.count
+  json.article_type photo_url.blank? ? 1 : 2
+  json.photo_url photo_url
 end
